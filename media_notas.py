197d01fcee183ec5854c566a41e501b843ca0551
Lucas de Alencar <lucasalvesalencar3@gmail.com>
2025-10-09 15:56:29 -0300

    Update media_notas.py

--- media_notas.py
+++ media_notas.py
@@ -1,20 +1,48 @@
-#Sitemas de notas do Suap
+#função
+def validação(nota1: int, nota2: int, nota3:int) -> bool:
+    if nota1 >=0 or nota1 <=100 and nota2 >= 0 or nota2 <=100 and nota3 >=0 or nota3<=100:
+        print('notas válidas!')
+        return True
+    else:
+        print('notas inválidas, tente novamente!')
+    return False
+
+def media_arit(nota1: int, nota2: int, nota3: int) -> bool:
+       return (nota1 + nota2 + nota3) / 3
+def aprovado_ou_nao(nota1: int, nota2: int, nota3: int) -> bool:
+    if media >= 70:
+        print(f'A media do aluno(a) {nome} foi {media:.2f}, está aprovado! Parabéns!')
+        return True
+    else:
+        print(f'A média do aluno(a) {nome} foi de {media:.2f}, está reprovado...')
+        return False
+
+
+
+#Sitemas de notas do Suap(código principal)
 nome = input('Nome do aluno: ')
 print('-----Média de notas do suap-----')
 nota1 = float(input('digite sua primeira nota:' ))
 nota2 = float(input('digite sua segunda nota: '))
 nota3 = float(input('Digite sua terceira nota: '))
+if validação(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) / 3
+aprovado_ou_nao(nota1, nota2, nota3)
 
-#validação das notas
-if nota1 and nota2 and nota3 >= 0:
-    print('Notas válidas!')
-else:
-    print('Notas inválidas! Por favor digite novamente.')
 
-#calcular média
-media = (nota1 + nota2 +nota3)/3
-print(f'Sua média final foi: {media}')
-if media >= 70:
-    print(f'O aluno, {nome} está aprovado!')
-else:
-    print(f'O aluno {nome}, está reprovado...')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
